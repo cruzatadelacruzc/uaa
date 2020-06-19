bf10e0b66d1fdc90abe69695daf38b92aed902fc
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2020-06-19 12:37:26 -0400

    Add error_concurrency

--- src/main/java/com/example/demo/web/rest/error/ErrorConstants.java
+++ src/main/java/com/example/demo/web/rest/error/ErrorConstants.java
@@ -5,6 +5,7 @@ import java.net.URI;
 final class ErrorConstants {
 
     private static final String PROBLEM_BASE_URL = "http://localhost:8080/problem";
+    public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
     static final String ERR_VALIDATION = "error.validation";
     static final URI DEFAULT_TYPE = URI.create(PROBLEM_BASE_URL + "/problem-with-message");
     static final URI ENTITY_NOT_FOUND_TYPE = URI.create(PROBLEM_BASE_URL + "/entity-not-found");
